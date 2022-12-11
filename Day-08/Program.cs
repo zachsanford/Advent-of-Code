470e6a97ac4f751e28edc09b4bbae8f1be53e76b
Zach Sanford <sanfordzachary@gmail.com>
2022-12-11 07:36:43 -0800

    Made left checking functionality.

--- Day-08/Program.cs
+++ Day-08/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// Some changes
+﻿string[] rawData = File.ReadAllLines(@"Data.txt");
+List<int[]> tempStructure = new();
+int totalVisible = 392; // Starting with the outer perimeter
+
+foreach (string rD in rawData)
+{
+    List<int> tempEntry = new();
+    foreach (char _rd in rD)
+    {
+        tempEntry.Add(Convert.ToInt32(_rd.ToString()));
+    }
+    tempStructure.Add(tempEntry.ToArray());
+}
+
+// dataStructure[ROW][COLUMN]
+int[][] dataStructure = tempStructure.ToArray();
+tempStructure.Clear();
+
+Console.WriteLine(IsVisibleLeft(1, 1));
+
+#region Methods
+
+bool IsVisibleLeft(int _posRow, int _posCol)
+{
+    int _currentPos = dataStructure[_posRow][_posCol];
+    bool _isVisible = true;
+
+    for (int i = _posCol - 1; i >= 0; i--)
+    {
+        if (_currentPos <= dataStructure[_posRow][i])
+        {
+            _isVisible = false;
+            break;
+        }
+    }
+
+    return _isVisible;
+}
+
+#endregion
